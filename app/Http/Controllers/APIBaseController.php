bc17bf4fe68f93e8e56af889f3857657ee18a4dd
acxe21 <jeromedacasin21@gmail.com>
2025-02-22 22:31:44 +0800

    remove the dd in error message

--- app/Http/Controllers/APIBaseController.php
+++ app/Http/Controllers/APIBaseController.php
@@ -73,7 +73,6 @@ class APIBaseController extends Controller implements APIInterface
                 'message'     => 'Unable to find requested data'
             ], 404);
         } catch (Exception $e) {
-            dd($e);
             return response()
             ->json([
                 'status_code' => $e->getCode(),
